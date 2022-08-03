6496c44f5ad7e01a6210297681ddd15df161a920
moringa <oscarokumu47@gmail.com>
2022-08-03 19:23:28 +0300

    modifiy index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string){
+  return "hello".toUpperCase();
+
+}
+function whisper(string){
+    return "HELLO".toLowerCase();
+}
+function logShout(string){
+    console.log("hello".toUpperCase());
+}
+function logWhisper(string){
+    console.log("HELLO".toLowerCase());
+} 
+function sayHiToHeadphonedRoommate(string){
+    console.log("HELLO".toLowerCase())
+    if (string=true){
+        return "I can\'t hear you!";
+    }
+}
+function sayHiToHeadphonedRoommate(string){
+    console.log("hello".toUpperCase())
+    if (string=true){
+        return 
+    }
+}
